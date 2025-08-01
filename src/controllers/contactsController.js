b45f89b11dfce7935dbe763349668c05bda168dd
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-08-01 07:58:59 +0200

    update

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -3,16 +3,26 @@ import { patchContact } from '../controllers/contacts.js';
 
 export const getContacts = async (req, res, next) => {
   try {
+    const userId = req.user._id; // здесь req.user должен быть установлен middleware аутентификации
+
     const {
       page = 1,
       perPage = 10,
       sortBy = 'name',
       sortOrder = 'asc',
-      type,           // query параметр для фільтрації по contactType
-      isFavourite,    // query параметр для фільтрації по isFavourite
+      type,
+      isFavourite,
     } = req.query;
 
-    const result = await getAllContacts({ page, perPage, sortBy, sortOrder, type, isFavourite });
+    const result = await getAllContacts({
+      userId,
+      page,
+      perPage,
+      sortBy,
+      sortOrder,
+      type,
+      isFavourite,
+    });
 
     res.status(200).json({
       status: 200,
@@ -26,31 +36,6 @@ export const getContacts = async (req, res, next) => {
         hasPreviousPage: result.hasPreviousPage,
         hasNextPage: result.hasNextPage,
       },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-
-
-export const getContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      return res.status(404).json({
-        status: 404,
-        message: 'Contact not found',
-        data: null,
-      });
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
     });
   } catch (error) {
     next(error);
