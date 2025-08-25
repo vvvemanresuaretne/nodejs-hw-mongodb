59637ffe423995ae45ebc37b64a757ce40405cd4
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-08-25 20:16:03 +0200

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,6 +7,13 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Визначаємо кореневу папку проєкту (папка, в якій лежить src)
+const ROOT_DIR = path.resolve(__dirname, '..'); // Якщо config.js у src/constants, то ../ веде до src
+
+// Тепер визначаємо шлях до папки з шаблонами, відносно кореня проєкту
+export const TEMPLATES_DIR = path.join(ROOT_DIR, 'templates');
+
+// Конфігурація SMTP
 export const SMTP = {
   SMTP_HOST: process.env.SMTP_HOST,
   SMTP_PORT: process.env.SMTP_PORT,
@@ -15,8 +22,7 @@ export const SMTP = {
   SMTP_FROM: process.env.SMTP_FROM,
 };
 
-export const TEMPLATES_DIR = path.join(ROOT_DIR, 'src', 'templates');
-
+// Конфігурація Cloudinary
 export const CLOUDINARY = {
   CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   API_KEY: process.env.CLOUDINARY_API_KEY,
